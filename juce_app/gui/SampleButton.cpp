566ca94c8199c4721f7c806e1e21d220e32a4c25
justindx7 <justin.albonnet@gmail.com>
2025-06-11 17:30:09 +0200

    added custom TextButton that can load samples

---
+++ juce_app/gui/SampleButton.cpp
@@ -0,0 +1,88 @@
+#include "SampleButton.h"
+#include "juce_core/system/juce_PlatformDefs.h"
+
+SampleButton::SampleButton(const juce::String& buttonText)
+    : juce::TextButton(buttonText)
+{
+    //setColour(juce::TextButton::buttonColourId, juce::Colours::darkgrey);
+    //setColour(juce::TextButton::textColourOnId, juce::Colours::white);
+    //setColour(juce::TextButton::textColourOffId, juce::Colours::white);
+}
+
+SampleButton::~SampleButton() = default;
+
+void SampleButton::mouseEnter(const juce::MouseEvent& event)
+{
+    //setColour(juce::TextButton::buttonColourId, juce::Colours::deepskyblue);
+    repaint();
+    juce::TextButton::mouseEnter(event);
+}
+
+void SampleButton::mouseExit(const juce::MouseEvent& event)
+{
+    //setColour(juce::TextButton::buttonColourId, juce::Colours::darkgrey);
+    repaint();
+    juce::TextButton::mouseExit(event);
+}
+
+
+void SampleButton::setEditMode(bool shouldBeInEditMode) {
+    editMode = shouldBeInEditMode;
+}
+
+bool SampleButton::isInEditMode() const {
+    return editMode;
+}
+
+void SampleButton::enterEditMode() {
+
+fileChooser = std::make_unique<FileChooser> ("Please select the audio file you want to load...",
+                                               File::getSpecialLocation (File::userHomeDirectory),
+                                               audioFileWildcard);
+ 
+    auto fileChooserFlags = FileBrowserComponent::openMode | FileBrowserComponent::canSelectFiles;
+
+    fileChooser->launchAsync(fileChooserFlags, [this](
+                                                   const FileChooser &chooser) {
+      File selectedFile(chooser.getResult());
+
+      if (selectedFile.existsAsFile()) {
+        selectedFilePath = selectedFile.getFullPathName();
+
+        setButtonText(selectedFile.getFileName());
+        repaint();
+        DBG( "Selected file path: " << selectedFilePath.toStdString());
+
+        // Call the callback if it's set
+        if (onFileSelected)
+          onFileSelected(selectedFilePath);
+      } else {
+        DBG("No file selected");
+      }
+    });
+}
+
+void SampleButton::clicked()
+{
+    if (editMode)
+    {
+        enterEditMode();
+        DBG("Opening Edit Mode");
+        if (onEditModeClick)
+            onEditModeClick();
+            
+    }
+    else
+    {
+        if (onNormalClick)
+            onNormalClick();
+    }
+}
+
+void SampleButton::setFileSelectedCallback(std::function<void(juce::String)> callback) {
+  onFileSelected = callback;
+}
+
+juce::String SampleButton::getSelectedFilePath() const {
+  return selectedFilePath;
+}
---
+++ juce_app/gui/SampleButton.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class SampleButton : public juce::TextButton {
+public:
+    SampleButton(const juce::String& buttonText);
+    ~SampleButton () override;
+
+    void mouseEnter(const juce::MouseEvent& event) override;
+    void mouseExit(const juce::MouseEvent& event) override;
+    void clicked() override;
+
+    void setEditMode(bool shouldBeInEditMode);
+    bool isInEditMode() const;
+    void enterEditMode();
+
+    void setFileSelectedCallback(std::function<void(juce::String)> callback);
+    juce::String getSelectedFilePath() const;
+
+    std::function<void()> onEditModeClick;
+    std::function<void()> onNormalClick;
+
+private:
+    juce::String audioFileWildcard = "*.wav;*.mp3;*.aiff;*.flac;*.ogg";
+    std::unique_ptr<juce::FileChooser> fileChooser;
+    bool editMode = false;
+
+    juce::String selectedFilePath;
+    std::function<void(juce::String)> onFileSelected;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SampleButton )
+};
